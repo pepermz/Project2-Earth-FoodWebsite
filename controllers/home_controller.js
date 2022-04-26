49274c05f00fa763cd90d9905c60846287649d1e
pepermz <jose.ram.18@gmail.com>
2022-04-26 10:12:42 -0500

    created post route

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
+
+//  localhost:4000/home 
 router.get('/', async (req, res, next) => {
     try {
         const posts = await db.Post.find({});
@@ -15,11 +17,57 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+// localhost:4000/home/new    
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+
+// localhost:4000/home/:id
+router.get('/:id/', async (req, res, next) => {
+    try {
+        const foundPost = await db.Post.findById(req.params.id)
+        console.log(foundPost);
+        const context = { 
+            onePost: foundPost,
+            message: "Hello there"
+        }
+        return res.render('show.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
 
 
+// POST ROUTE
+router.post('/', async (req, res, next) => {
+    try {
+        // console.log(`The req.body is ${req.body}`)
+        const createdPost = await db.Post.create(req.body);
+        console.log(`The created post is ${createdPost}`)
+        res.redirect('/home');
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
-
+// UPDATE PUT ROUTE
+router.put('/:id', async (req, res, next)=>{
+    try {
+        const updatedPost = await db.Post.findByIdAndUpdate(req.params.id, req.body);
+        console.log(updatedPost);
+        return res.redirect(`/home`)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
 
 
