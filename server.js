16058e1c86f533edf42d13f49d3dd566bb44b11f
pepermz <jose.ram.18@gmail.com>
2022-04-26 09:13:42 -0500

    server file running

--- server.js
+++ server.js
@@ -7,18 +7,25 @@ const PORT = 4000;
 //DB Connection
 require('./config/db.connection')
 
+//app configs
+app.set('view engine', 'ejs')
+
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false}))
+
+// CONTROLLERS
+app.use('/home', controllers.home)
+
+
+app.get('/', (request, response) => response.send('Welcome to Foodapp'))
 
 
 
 
 
-
-
-
-
-
-
-
+// PORT
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
